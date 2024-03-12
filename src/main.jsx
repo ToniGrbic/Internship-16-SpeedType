41e7e2a9071daeacbff2fce703c416c2284285c8
ToniGrbic <tonigrbic.5@gmail.com>
2024-03-12 22:36:18 +0100

    dialog provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import DialogProvider from "./providers/DialogProvider.jsx";
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <DialogProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </DialogProvider>
   </React.StrictMode>
 );
---
+++ src/providers/DialogProvider.jsx
@@ -0,0 +1,43 @@
+import React, { useContext, useState } from "react";
+
+export const DIALOG = {
+  PRACTICE: "PRACTICE",
+  NEW_GAME: "NEW_GAME",
+  RESULTS: "RESULTS",
+  NEW_LEVEL: "NEW_LEVEL",
+};
+const defaultContext = {
+  activeDialog: null,
+  open: () => {},
+  close: () => {},
+  additionalProps: {},
+};
+
+const DialogContext = React.createContext(defaultContext);
+
+const DialogProvider = ({ children }) => {
+  const [activeDialog, setActiveDialog] = useState(null);
+  const [additionalProps, setAdditionalProps] = useState({});
+
+  const open = (dialog, props) => {
+    setActiveDialog(dialog);
+    setAdditionalProps(props);
+  };
+
+  const close = () => {
+    setActiveDialog(null);
+    setAdditionalProps({});
+  };
+
+  return (
+    <DialogContext.Provider
+      value={{ activeDialog, open, close, additionalProps }}
+    >
+      {children}
+    </DialogContext.Provider>
+  );
+};
+
+export const useDialog = () => useContext(DialogContext);
+
+export default DialogProvider;
