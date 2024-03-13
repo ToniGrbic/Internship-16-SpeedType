f7988445e1b0a95750cf5e0a19a61b0152897319
ToniGrbic <tonigrbic.5@gmail.com>
2024-03-13 20:07:38 +0100

    on submit functions

--- src/components/Dialogs/NewGameDialog.jsx
+++ src/components/Dialogs/NewGameDialog.jsx
@@ -10,17 +10,24 @@ import {
 
 import CloseIcon from "@mui/icons-material/Close";
 
-const NewGameDialog = ({ isOpen, onClose }) => {
+const NewGameDialog = ({ isOpen, onClose, onSubmit }) => {
   return (
     <Dialog open={isOpen}>
-      <IconButton onClick={onClose}>
+      <IconButton
+        sx={{ position: "absolute", top: "10px", right: "10px" }}
+        onClick={onClose}
+      >
         <CloseIcon />
       </IconButton>
       <DialogTitle>New Game</DialogTitle>
       <DialogContent>Select game mode</DialogContent>
       <DialogActions>
-        <Button color="secondary">Regular</Button>
-        <Button color="primary">Instant death</Button>
+        <Button onClick={onSubmit} color="secondary">
+          Regular
+        </Button>
+        <Button onClick={onSubmit} color="primary">
+          Instant death
+        </Button>
       </DialogActions>
     </Dialog>
   );
--- src/components/Dialogs/PracticeDialog.jsx
+++ src/components/Dialogs/PracticeDialog.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Button, Dialog, DialogActions, DialogTitle } from "@mui/material";
-const PracticeDialog = ({ isOpen, onClose }) => {
+
+const PracticeDialog = ({ isOpen, onClose, onSubmit }) => {
   return (
     <Dialog open={isOpen}>
       <DialogTitle>Do you want to enter Practice mode?</DialogTitle>
       <DialogActions>
-        <Button color="primary">YES</Button>
+        <Button onClick={onSubmit} color="primary">
+          YES
+        </Button>
         <Button onClick={onClose} color="secondary">
           NO
         </Button>
