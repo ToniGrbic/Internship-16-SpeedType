47da08eedb7ee579bb8a8d7688ad2bc5ecbd17b6
ToniGrbic <tonigrbic.5@gmail.com>
2024-03-18 00:15:55 +0100

    minor refactor and fix for handleSubmit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,21 +6,27 @@ import { Box, Tab, Tabs } from "@mui/material";
 import DialogSwitch from "../Dialogs/DialogSwitch";
 import { practiceDialogText } from "../../utils/constants";
 import styles from "./index.module.css";
+import { useStopWatch } from "../../providers/StopWatchProvider";
 
 const Header = () => {
   const navigate = useNavigate();
+  const { reset } = useStopWatch();
   const { open, close } = useDialog();
-  const { selectTexts, setGameType, totalWordsPerMinute } = useGame();
+  const { selectTexts, setGameType, resetGame, totalWordsPerMinute } =
+    useGame();
+
   const [value, setValue] = useState(0);
 
-  const handleSubmit = (route, gameType) => {
+  const handleSubmit = (route, gameType = null) => {
     close();
+    reset();
+    resetGame();
     setGameType(gameType);
+    selectTexts();
     navigate(route);
   };
 
   const openNewGame = () => {
-    selectTexts();
     open(DIALOG.NEW_GAME, {
       onSubmitRegular: () => {
         handleSubmit("/new-game", GAME_TYPE.REGULAR);
@@ -38,11 +44,11 @@ const Header = () => {
     });
   };
 
-  const options = [() => navigate("/"), openPractice, openNewGame];
+  const navigationOptions = [() => navigate("/"), openPractice, openNewGame];
 
-  const handleChange = (event, index) => {
+  const handleChange = (e, index) => {
     setValue(index);
-    options[index]();
+    navigationOptions[index]();
   };
 
   function a11yProps(index) {
