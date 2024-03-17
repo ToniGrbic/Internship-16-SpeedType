b55cda3b1be848e2c191c31511a6ef3f5644d1a0
ToniGrbic <tonigrbic.5@gmail.com>
2024-03-17 23:23:31 +0100

    moved formatTime to StopWatchProvider

--- src/components/Stopwatch/Stopwatch.jsx
+++ src/components/Stopwatch/Stopwatch.jsx
@@ -3,21 +3,11 @@ import { useStopWatch } from "../../providers/StopWatchProvider";
 import styles from "./index.module.css";
 
 const Stopwatch = () => {
-  const { elapsedTime } = useStopWatch();
-
-  const formatTime = () => {
-    let minutes = Math.floor((elapsedTime / (1000 * 60)) % 60);
-    let seconds = Math.floor((elapsedTime / 1000) % 60);
-
-    minutes = String(minutes).padStart(2, "0");
-    seconds = String(seconds).padStart(2, "0");
-
-    return `${minutes}:${seconds}`;
-  };
+  const { formatTime } = useStopWatch();
 
   return (
-    <div className="stopwatch">
-      <div className="display">{formatTime()}</div>
+    <div>
+      <div>{formatTime()}</div>
     </div>
   );
 };
