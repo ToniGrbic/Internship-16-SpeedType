bfb36027018c3a16453f451d294e259c789d4b81
ToniGrbic <tonigrbic.5@gmail.com>
2024-03-17 23:21:42 +0100

    disable btn if incorrect chars exist

--- src/components/Buttons/NextButton/NextButton.jsx
+++ src/components/Buttons/NextButton/NextButton.jsx
@@ -5,15 +5,20 @@ import { Button } from "@mui/material";
 import ArrowForward from "@mui/icons-material/ArrowForward";
 import { numberOfTexts } from "../../../utils/textPicker";
 
-const NextButton = ({ phase }) => {
+const NextButton = ({ phase, errorChar }) => {
   const { level, selectedTexts, nextLevelClick } = useGame();
+
   return (
     <Button
       variant="contained"
       color="primary"
       onClick={() => nextLevelClick(selectedTexts)}
       endIcon={<ArrowForward />}
-      disabled={level === numberOfTexts - 1 || phase !== PhaseType.Ended}
+      disabled={
+        level === numberOfTexts - 1 ||
+        phase !== PhaseType.Ended ||
+        errorChar !== 0
+      }
     >
       next
     </Button>
