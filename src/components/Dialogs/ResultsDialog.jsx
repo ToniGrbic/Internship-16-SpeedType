57517710944a1621b132cb2d75e65c19ba41dedf
ToniGrbic <tonigrbic.5@gmail.com>
2024-03-17 23:22:03 +0100

    stopwatch game time added

--- src/components/Dialogs/ResultsDialog.jsx
+++ src/components/Dialogs/ResultsDialog.jsx
@@ -7,14 +7,18 @@ import {
   DialogContent,
 } from "@mui/material";
 import { useGame } from "../../providers/GameProvider";
+import { useStopWatch } from "../../providers/StopWatchProvider";
 
 const ResultsDialog = ({ isOpen, onClose, onSubmit }) => {
   const { gameWordsPerMinute, totalWordsPerMinute } = useGame();
+  const { formatTime } = useStopWatch();
   return (
     <Dialog open={isOpen}>
       <DialogTitle>Results</DialogTitle>
       <DialogContent>
         WPM: {gameWordsPerMinute}
+        <br />
+        Time: {formatTime()}
         <br />
         Total WPM: {totalWordsPerMinute}
       </DialogContent>
