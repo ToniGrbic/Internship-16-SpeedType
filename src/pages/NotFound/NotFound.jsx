dcb75e0926750f0e00d535cb9aca33797c46a6a3
ToniGrbic <tonigrbic.5@gmail.com>
2024-03-18 16:12:35 +0100

    NotFound page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import styles from "./index.module.css";
+import BackButton from "../../components/Buttons/BackButton";
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div className={styles["not-found-container"]}>
+      <p className={styles["not-found-text"]}>404 Error: Page not found</p>
+      <BackButton />
+    </div>
+  );
 };
 
 export default NotFound;
