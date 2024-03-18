5687e1b5de2b9f96ab653eced0fe05fd61e4f04f
ToniGrbic <tonigrbic.5@gmail.com>
2024-03-18 21:51:33 +0100

    error state if typing text contains incorrect chars

--- src/components/TypingText/TypingText.jsx
+++ src/components/TypingText/TypingText.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./index.module.css";
 import { useGame } from "../../providers/GameProvider";
-import { PhaseType } from "react-typing-game-hook";
+import { PhaseType, CharStateType } from "react-typing-game-hook";
 import { useStopWatch } from "../../providers/StopWatchProvider";
 
 const TypingText = ({
@@ -15,49 +15,74 @@ const TypingText = ({
   resetTyping,
 }) => {
   const textEl = useRef(null);
+  const [isError, setIsError] = useState(false);
   const { level } = useGame();
   const { startClock } = useStopWatch();
+
+  const hasIncorrectChars = (key) => {
+    return (
+      charsState.includes(CharStateType.Incorrect) ||
+      key !== chars[currIndex + 1]
+    );
+  };
 
   const handleKeyDown = (e) => {
     const key = e.key;
     if (phase === PhaseType.NotStarted) startClock();
+
     if (key === "Escape") {
       resetTyping();
     } else if (key === "Backspace") {
       deleteTyping(false);
     } else if (key.length === 1) {
+      if (hasIncorrectChars(key) && currIndex + 2 === chars.length) {
+        setIsError(true);
+        return;
+      }
+      setIsError(false);
       insertTyping(key);
     }
     e.preventDefault();
   };
+
   useEffect(() => {
     textEl.current.focus();
   }, [level]);
 
   return (
-    <h2
-      className={styles.typingText}
-      ref={textEl}
-      onKeyDown={handleKeyDown}
-      tabIndex={0}
-    >
-      {chars.split("").map((char, index) => {
-        const state = charsState[index];
-        const color = state === 0 || state === 2 ? "black" : "green";
-        const backgroundColor = state === 2 ? "#ff8080" : "transparent";
-        return (
-          <span
-            key={index + id}
-            style={{ backgroundColor, color }}
-            className={`${styles["letter-span"]} ${
-              currIndex === index ? styles.currentLetter : ""
-            }`}
-          >
-            {char}
-          </span>
-        );
-      })}
-    </h2>
+    <>
+      <p
+        className={styles["incorrect-chars-message"]}
+        style={{ color: isError ? "red" : "transparent" }}
+      >
+        Text must have all correct chars to finish the level!
+      </p>
+      <h2
+        className={styles.typingText}
+        ref={textEl}
+        onKeyDown={handleKeyDown}
+        tabIndex={0}
+      >
+        {chars.split("").map((char, index) => {
+          const state = charsState[index];
+          const color = state === CharStateType.Correct ? "green" : "black";
+          const backgroundColor =
+            state === CharStateType.Incorrect ? "#ff8080" : "transparent";
+
+          return (
+            <span
+              key={index + id}
+              style={{ backgroundColor, color }}
+              className={`${styles["letter-span"]} ${
+                currIndex === index ? styles.currentLetter : ""
+              }`}
+            >
+              {char}
+            </span>
+          );
+        })}
+      </h2>
+    </>
   );
 };
 
